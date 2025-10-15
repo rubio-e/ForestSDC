e75248c44ac09d35987e891c6192dd47d0c6461b
rubio-e <ernestorub@gmail.com>
2025-10-15 15:14:23 -0600

    minor changes

--- R/ca.R
+++ R/ca.R
@@ -1,18 +1,18 @@
-#' Individual Tree Crown Area
-#'
-#' This function calculates the crown area of an individual tree based on two
-#' measurements of its crown diameters. The crown area is computed using the
-#' average of the two diameters and is assumed to be circular.
-#'
-#' @param cw_a A numeric variable representing the first tree crown diameter in meters.
-#' @param cw_b A numeric variable representing the second tree crown diameter in meters.
-#'
-#' @return A numeric variable representing the tree crown area in square meters.
-#'
-#' @examples
-#' crown_area <- ca(10, 15)
-#' crown_area # Output: Area in square meters
-#'
+# Individual Tree Crown Area
+#
+# This function calculates the crown area of an individual tree based on two
+# measurements of its crown diameters. The crown area is computed using the
+# average of the two diameters and is assumed to be circular.
+#
+# @param cw_a A numeric variable representing the first tree crown diameter in meters.
+# @param cw_b A numeric variable representing the second tree crown diameter in meters.
+#
+# @return A numeric variable representing the tree crown area in square meters.
+#
+# @examples
+# crown_area <- ca(10, 15)
+# crown_area # Output: Area in square meters
+#
 #' @keywords internal
 ca <- function(cw_a, cw_b) {
   x <- cw_a
--- R/rich.R
+++ R/rich.R
@@ -1,19 +1,19 @@
-#' Calculate Species Richness (S)
-#'
-#' This function calculates species richness (\eqn{S}), which represents the
-#' number of unique species or categories in the given data. The formula is:
-#' \deqn{S = \sum I(counts > 0)}
-#' where:
-#' - \eqn{counts} represents the frequency of each unique species or category.
-#' @md
-#' @param sp Numeric or factor variable. A vector representing species or categories.
-#'
-#' @return Numeric. The species richness (\eqn{S}).
-#'
-#' @examples
-#' x <- c("n", "m", "n", "n", "m")
-#' rich(x)
-#'
+# Calculate Species Richness (S)
+#
+# This function calculates species richness (\eqn{S}), which represents the
+# number of unique species or categories in the given data. The formula is:
+# \deqn{S = \sum I(counts > 0)}
+# where:
+# - \eqn{counts} represents the frequency of each unique species or category.
+# @md
+# @param sp Numeric or factor variable. A vector representing species or categories.
+#
+# @return Numeric. The species richness (\eqn{S}).
+#
+# @examples
+# x <- c("n", "m", "n", "n", "m")
+# rich(x)
+#
 #' @keywords internal
 rich <- function(sp) {
   x <- sp
